42587763e070317342afaf9d697fdf99ed3be2f5
Asmatchd <Asmatchd@gmail.com>
2021-06-04 17:53:17 +0500

    borders, alignments done

--- App.js
+++ App.js
@@ -8,16 +8,23 @@ class App extends React.Component {
     return (
       <View
         style={{
-          backgroundColor: '#faf',
+          backgroundColor: '#faf6',
           //height: 500,
           //width: 200,
           flex: 1,
+          alignItems: 'center',
+          // alignItems: 'flex-end',
+          justifyContent: 'center',
+          // justifyContent: 'flex-end',
         }}>
         <View
           style={{
             backgroundColor: '#aaf',
             height: 100,
             width: 100,
+            borderWidth: 2,
+            borderColor: '#aff',
+            borderRadius: 50,
           }}></View>
 
         <View
@@ -25,13 +32,54 @@ class App extends React.Component {
             backgroundColor: '#aff',
             height: 150,
             width: 150,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderLeftColor: 'red',
+            borderTopColor: 'rgba(31, 58, 147, 0.5)',
+            borderRightColor: '#0004',
+            borderBottomColor: 'rgba(0,0,0,0.05)',
+            // borderBottomColor: '#0001',
+            borderLeftWidth: 2,
+            borderTopWidth: 5,
+            borderRightWidth: 10,
+            borderBottomWidth: 20,
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 50,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 5,
           }}>
           <View
             style={{
-              backgroundColor: 'black',
-              height: 50,
-              width: 50,
-            }}></View>
+              backgroundColor: '#fff',
+              height: 100,
+              width: 100,
+              borderWidth: 0.5,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 50,
+            }}>
+            <View
+              style={{
+                backgroundColor: '#fff',
+                height: 70,
+                width: 70,
+                borderWidth: 1,
+                borderColor: 'red',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 35,
+              }}>
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  height: 50,
+                  width: 50,
+                  borderWidth: 1,
+                  borderColor: 'blue',
+                  borderRadius: 25,
+                }}></View>
+            </View>
+          </View>
         </View>
       </View>
     );
