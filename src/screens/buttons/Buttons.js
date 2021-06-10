b442306f9616e2b1d546a146e74b976efee5eab8
Asmatchd <Asmatchd@gmail.com>
2021-06-10 18:00:13 +0500

    textinput done

--- src/screens/buttons/Buttons.js
+++ src/screens/buttons/Buttons.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {View, Text, Button, TouchableOpacity, TextInput} from 'react-native';
 
 export class Buttons extends React.Component {
+  state = {
+    firstValue: '',
+    secondValue: '10',
+  };
+
   firstFunction = data => {
     if (data === '1') {
       console.warn('1 Pressed');
@@ -23,11 +28,18 @@ export class Buttons extends React.Component {
   };
 
   combined = (a, b, fn) => {
+    // if (fn === 'add') {
+    //   const ans = a + b;
+    //   console.warn('after addition answer is ' + ans);
+    // } else {
+    //   const ans = a - b;
+    //   console.warn('after subtraction answer is ' + ans);
+    // }
     if (fn === 'add') {
-      const ans = a + b;
+      const ans = +this.state.firstValue + +this.state.secondValue;
       console.warn('after addition answer is ' + ans);
     } else {
-      const ans = a - b;
+      const ans = +this.state.firstValue - +this.state.secondValue;
       console.warn('after subtraction answer is ' + ans);
     }
   };
@@ -36,7 +48,7 @@ export class Buttons extends React.Component {
     return (
       <View
         style={{
-          backgroundColor: '#0002',
+          backgroundColor: '#fff',
           //height: 500,
           //width: 200,
           flex: 1,
@@ -132,6 +144,61 @@ export class Buttons extends React.Component {
           }}>
           <Text>Subtract</Text>
         </TouchableOpacity>
+
+        <TextInput
+          // onChangeText={txt => {
+          //   this.setState({firstValue: txt});
+          // }}
+          onChangeText={firstValue => {
+            this.setState({firstValue});
+          }}
+          style={{
+            // height: 50,
+            width: '95%',
+            backgroundColor: '#0001',
+            borderWidth: 0.5,
+            borderColor: 'red',
+            marginTop: 5,
+            borderRadius: 5,
+            fontSize: 20,
+            color: 'blue',
+          }}
+          placeholder={'First number'}
+          placeholderTextColor={'red'}
+          numberOfLines={5}
+          // minHeight={50}
+          // maxHeight={200}
+          multiline
+          keyboardType={'number-pad'}
+          textAlignVertical={'top'}
+          value={this.state.firstValue}
+          // for password entry  uncomment  this
+          // secureTextEntry
+        />
+
+        <TextInput
+          // onChangeText={txt => {
+          //   this.setState({firstValue: txt});
+          // }}
+          onChangeText={secondValue => {
+            this.setState({secondValue});
+          }}
+          style={{
+            height: 40,
+            width: '95%',
+            backgroundColor: '#0001',
+            borderWidth: 0.5,
+            borderColor: 'red',
+            marginTop: 5,
+            borderRadius: 5,
+          }}
+          placeholder={'Second number'}
+          // numberOfLines={2}
+          // multiline={true}
+          keyboardType={'phone-pad'}
+          value={this.state.secondValue}
+          editable={false}
+        />
       </View>
     );
   }
