5ee6134534457d9e022d18de9f53a57ec0606ac8
Asmatchd <Asmatchd@gmail.com>
2021-06-14 18:02:06 +0500

    keyboard aware and responsive screen done

--- src/screens/signIn/SignIn.js
+++ src/screens/signIn/SignIn.js
@@ -12,291 +12,304 @@ import {
 import Icon from 'react-native-vector-icons/Ionicons';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import Entypo from 'react-native-vector-icons/Entypo';
-
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {
+  widthPercentageToDP as w,
+  heightPercentageToDP as h,
+} from 'react-native-responsive-screen';
 export class SignIn extends React.Component {
   state = {};
 
   render() {
     return (
-      <ImageBackground
-        source={require('../../assets/bg.jpg')}
-        style={{
-          height: '100%',
-          width: '100%',
+      <KeyboardAwareScrollView
+        contentContainerStyle={{
+          flexGrow: 2,
         }}>
-        <View
+        <ImageBackground
+          source={require('../../assets/bg.jpg')}
           style={{
-            //   backgroundColor: '#fff',
-            flex: 1,
+            height: '100%',
+            width: '100%',
           }}>
-          {/* back button */}
           <View
             style={{
-              //backgroundColor: '#faf',
-              height: 50,
-              justifyContent: 'center',
-              paddingLeft: 10,
+              backgroundColor: '#fff',
+              flex: 1,
             }}>
-            <Icon name={'ios-arrow-back'} size={25} color={'#000'} />
-          </View>
-          {/* top view */}
-          <View
-            style={{
-              height: 150,
-              //backgroundColor: '#aaf',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              paddingBottom: 15,
-            }}>
-            <Text
-              style={{
-                fontSize: 20,
-                fontWeight: 'bold',
-              }}>
-              Create Account
-            </Text>
-          </View>
-          {/* center view */}
-          <View
-            style={{
-              height: 300,
-              //backgroundColor: '#afa',
-              alignItems: 'center',
-              paddingBottom: 15,
-            }}>
-            {/* name */}
+            {/* back button */}
             <View
               style={{
-                backgroundColor: '#fff',
-                width: '90%',
-                height: 45,
-                flexDirection: 'row',
-                borderRadius: 25,
-                marginTop: 15,
-                borderWidth: 3,
-                borderTopWidth: 0,
-                borderColor: 'rgba(0,0,0,0.05)',
+                backgroundColor: '#faf',
+                height: h('6%'),
+                justifyContent: 'center',
+                paddingLeft: w('3%'),
               }}>
-              <View
-                style={{
-                  width: '15%',
-                  height: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Icon name={'person'} size={20} color={'black'} />
-              </View>
-              <TextInput
-                style={{
-                  // backgroundColor: '#ffa',
-                  width: '85%',
-                  height: '100%',
-                }}
-                placeholder={'Name'}
-              />
+              <Icon name={'ios-arrow-back'} size={h('4%')} color={'#000'} />
             </View>
-            {/* password */}
+            {/* top view */}
             <View
               style={{
-                backgroundColor: '#fff',
-                width: '90%',
-                height: 45,
-                flexDirection: 'row',
-                borderRadius: 25,
-                marginTop: 15,
-                borderWidth: 3,
-                borderTopWidth: 0,
-                borderColor: 'rgba(0,0,0,0.05)',
-              }}>
-              <View
-                style={{
-                  width: '15%',
-                  height: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Icon name={'lock-closed'} size={20} color={'black'} />
-              </View>
-              <TextInput
-                style={{
-                  // backgroundColor: '#ffa',
-                  width: '85%',
-                  height: '100%',
-                }}
-                placeholder={'Password'}
-                secureTextEntry
-              />
-            </View>
-            {/* email */}
-            <View
-              style={{
-                backgroundColor: '#fff',
-                width: '90%',
-                height: 45,
-                flexDirection: 'row',
-                borderRadius: 25,
-                marginTop: 15,
-                borderWidth: 3,
-                borderTopWidth: 0,
-                borderColor: 'rgba(0,0,0,0.05)',
-              }}>
-              <View
-                style={{
-                  width: '15%',
-                  height: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Icon name={'ios-mail'} size={20} color={'black'} />
-              </View>
-              <TextInput
-                style={{
-                  // backgroundColor: '#ffa',
-                  width: '85%',
-                  height: '100%',
-                }}
-                placeholder={'Email'}
-              />
-            </View>
-            {/* phone */}
-            <View
-              style={{
-                backgroundColor: '#fff',
-                width: '90%',
-                height: 45,
-                flexDirection: 'row',
-                borderRadius: 25,
-                marginTop: 15,
-                borderWidth: 3,
-                borderTopWidth: 0,
-                borderColor: 'rgba(0,0,0,0.05)',
-              }}>
-              <View
-                style={{
-                  width: '15%',
-                  height: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Icon name={'call'} size={20} color={'black'} />
-              </View>
-              <TextInput
-                style={{
-                  // backgroundColor: '#ffa',
-                  width: '85%',
-                  height: '100%',
-                }}
-                placeholder={'Phone'}
-              />
-            </View>
-          </View>
-          {/* bottom view */}
-          <View
-            style={{
-              height: 159,
-              // backgroundColor: '#faf',
-            }}>
-            <View
-              style={{
-                width: '100%',
+                height: h('20%'),
+                backgroundColor: '#aaf',
                 alignItems: 'center',
+                justifyContent: 'flex-end',
+                paddingBottom: h('2%'),
               }}>
+              <Text
+                style={{
+                  fontSize: h('3%'),
+                  fontWeight: 'bold',
+                }}>
+                Create Account
+              </Text>
+            </View>
+            {/* center view */}
+            <View
+              style={{
+                height: h('45%'),
+                backgroundColor: '#afa',
+                alignItems: 'center',
+                paddingBottom: h('2%'),
+              }}>
+              {/* name */}
               <View
                 style={{
-                  height: 50,
+                  backgroundColor: '#fff',
                   width: '90%',
-                  //backgroundColor: '#a3a',
+                  height: h('7%'),
                   flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'flex-end',
+                  borderRadius: h('4%'),
+                  marginTop: h('2%'),
+                  borderWidth: h('0.4%'),
+                  borderTopWidth: 0,
+                  borderColor: 'rgba(0,0,0,0.05)',
                 }}>
-                <Text
+                <View
                   style={{
-                    fontSize: 20,
-                    fontWeight: 'bold',
-                  }}>
-                  Create
-                </Text>
-                <TouchableOpacity
-                  style={{
-                    height: 30,
-                    width: 50,
-                    backgroundColor: 'orange',
+                    width: '15%',
+                    height: '100%',
                     alignItems: 'center',
                     justifyContent: 'center',
-                    borderRadius: 20,
-                    marginLeft: 10,
-                    // marginRight: 25,
                   }}>
-                  <Icon name={'ios-arrow-forward'} size={25} color={'#fff'} />
-                </TouchableOpacity>
+                  <Icon name={'person'} size={h('3%')} color={'black'} />
+                </View>
+                <TextInput
+                  style={{
+                    // backgroundColor: '#ffa',
+                    width: '85%',
+                    height: '100%',
+                  }}
+                  placeholder={'Name'}
+                />
+              </View>
+              {/* password */}
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  width: '90%',
+                  height: 45,
+                  flexDirection: 'row',
+                  borderRadius: 25,
+                  marginTop: 15,
+                  borderWidth: 3,
+                  borderTopWidth: 0,
+                  borderColor: 'rgba(0,0,0,0.05)',
+                }}>
+                <View
+                  style={{
+                    width: '15%',
+                    height: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Icon name={'lock-closed'} size={20} color={'black'} />
+                </View>
+                <TextInput
+                  style={{
+                    // backgroundColor: '#ffa',
+                    width: '85%',
+                    height: '100%',
+                  }}
+                  placeholder={'Password'}
+                  secureTextEntry
+                />
+              </View>
+              {/* email */}
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  width: '90%',
+                  height: 45,
+                  flexDirection: 'row',
+                  borderRadius: 25,
+                  marginTop: 15,
+                  borderWidth: 3,
+                  borderTopWidth: 0,
+                  borderColor: 'rgba(0,0,0,0.05)',
+                }}>
+                <View
+                  style={{
+                    width: '15%',
+                    height: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Icon name={'ios-mail'} size={20} color={'black'} />
+                </View>
+                <TextInput
+                  style={{
+                    // backgroundColor: '#ffa',
+                    width: '85%',
+                    height: '100%',
+                  }}
+                  placeholder={'Email'}
+                />
+              </View>
+              {/* phone */}
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  width: '90%',
+                  height: 45,
+                  flexDirection: 'row',
+                  borderRadius: 25,
+                  marginTop: 15,
+                  borderWidth: 3,
+                  borderTopWidth: 0,
+                  borderColor: 'rgba(0,0,0,0.05)',
+                }}>
+                <View
+                  style={{
+                    width: '15%',
+                    height: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Icon name={'call'} size={20} color={'black'} />
+                </View>
+                <TextInput
+                  style={{
+                    // backgroundColor: '#ffa',
+                    width: '85%',
+                    height: '100%',
+                  }}
+                  placeholder={'Phone'}
+                />
               </View>
             </View>
-
+            {/* bottom view */}
             <View
               style={{
-                width: '100%',
-                height: 50,
-                //   backgroundColor: '#a6a',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Text>Or create account using social media</Text>
-            </View>
-
-            <View
-              style={{
-                width: '100%',
-                height: 50,
-                //backgroundColor: '#a6a',
-                alignItems: 'center',
-                justifyContent: 'center',
+                height: h('25.5%'),
+                backgroundColor: '#faf',
               }}>
               <View
                 style={{
-                  width: '40%',
-                  //backgroundColor: '#fff',
-                  flexDirection: 'row',
-                  justifyContent: 'space-evenly',
+                  width: '100%',
+                  alignItems: 'center',
                 }}>
                 <View
                   style={{
-                    height: 30,
-                    width: 30,
-                    backgroundColor: 'blue',
-                    borderRadius: 15,
+                    height: h('7%'),
+                    width: '90%',
+                    backgroundColor: '#a3a',
+                    flexDirection: 'row',
                     alignItems: 'center',
-                    justifyContent: 'center',
+                    justifyContent: 'flex-end',
                   }}>
-                  <Fontisto name={'facebook'} size={15} color={'#fff'} />
+                  <Text
+                    style={{
+                      fontSize: h('2.5%'),
+                      fontWeight: 'bold',
+                    }}>
+                    Create
+                  </Text>
+                  <TouchableOpacity
+                    style={{
+                      height: h('4%'),
+                      width: w('15%'),
+                      backgroundColor: 'orange',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      borderRadius: h('2%'),
+                      marginLeft: w('2%'),
+                      // marginRight: 25,
+                    }}>
+                    <Icon
+                      name={'ios-arrow-forward'}
+                      size={h('4%')}
+                      color={'#fff'}
+                    />
+                  </TouchableOpacity>
                 </View>
+              </View>
+
+              <View
+                style={{
+                  width: '100%',
+                  height: h('6%'),
+                  // backgroundColor: '#a6a',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text>Or create account using social media</Text>
+              </View>
+
+              <View
+                style={{
+                  width: '100%',
+                  height: h('8%'),
+                  backgroundColor: '#a6a',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
                 <View
                   style={{
-                    height: 30,
-                    width: 30,
-                    backgroundColor: 'rgba(25, 181, 254, 1)',
-                    borderRadius: 15,
-                    alignItems: 'center',
-                    justifyContent: 'center',
+                    width: '40%',
+                    backgroundColor: '#fff',
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
                   }}>
-                  <Icon name={'logo-twitter'} size={15} color={'#fff'} />
-                </View>
-                <View
-                  style={{
-                    height: 30,
-                    width: 30,
-                    backgroundColor: 'red',
-                    borderRadius: 15,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <Entypo name={'google-'} size={15} color={'#fff'} />
+                  <View
+                    style={{
+                      height: 30,
+                      width: 30,
+                      backgroundColor: 'blue',
+                      borderRadius: 15,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <Fontisto name={'facebook'} size={15} color={'#fff'} />
+                  </View>
+                  <View
+                    style={{
+                      height: 30,
+                      width: 30,
+                      backgroundColor: 'rgba(25, 181, 254, 1)',
+                      borderRadius: 15,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <Icon name={'logo-twitter'} size={15} color={'#fff'} />
+                  </View>
+                  <View
+                    style={{
+                      height: 30,
+                      width: 30,
+                      backgroundColor: 'red',
+                      borderRadius: 15,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <Entypo name={'google-'} size={15} color={'#fff'} />
+                  </View>
                 </View>
               </View>
             </View>
           </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+      </KeyboardAwareScrollView>
     );
   }
 }
